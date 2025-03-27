5b3ae18c71195856b12b171a0641db8c88565fbd
tathagat <tathagat.reimann@gmail.com>
2025-03-27 11:43:30 +0100

    logging with logger

--- tutorials/java/spring/springboot/reactive/src/main/java/com/cleancodecamp/spring/boot/reactive/service/SlowService.java
+++ tutorials/java/spring/springboot/reactive/src/main/java/com/cleancodecamp/spring/boot/reactive/service/SlowService.java
@@ -1,5 +1,7 @@
 package com.cleancodecamp.spring.boot.reactive.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -8,9 +10,10 @@ import java.util.Arrays;
 
 @Service
 public class SlowService {
+    Logger logger = LoggerFactory.getLogger(SlowService.class);
     public Flux<String> getAccounts() {
         return Flux.fromIterable(Arrays.asList("slow_1", "slow_2", "slow_3"))
                 .delayElements(Duration.ofSeconds(3))
-                .doOnNext(System.out::println);
+                .doOnNext(x -> logger.info(x));
     }
 }
